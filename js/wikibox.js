d8ba1e006e4780cc8f9a8b304afeb3fb7138221c
Schalk Neethling <sneethling@mozilla.com>
2021-03-15 19:28:55 +0200

    add opacity to current panel on transition

--- js/wikibox.js
+++ js/wikibox.js
@@ -1,12 +1,14 @@
 (function () {
   const wikibox = document.querySelector(".wikibox");
   const panelContainer = document.querySelector(".panel-container");
-  const numberOfPanels = panelContainer.querySelectorAll(".panel").length;
+  const panels = panelContainer.querySelectorAll(".panel");
+  const numberOfPanels = panels.length;
   const wikiboxWidth = wikibox.getBoundingClientRect().width;
   const startLeftPosition = 0;
   // ignore the initial panel
   const endLeftPosition = -((numberOfPanels - 1) * wikiboxWidth);
 
+  let currentPanel = panelContainer.querySelector(".panel:first-child");
   let panelContainerPosition = 0;
 
   function togglePagination() {
@@ -26,20 +28,31 @@
     }
   }
 
+  function handleTransition(panel) {
+    panel.addEventListener("transitionend", () => {
+      panel.classList.remove("transparent");
+    });
+  }
+
   if (wikibox) {
     wikibox.addEventListener("click", (event) => {
+      currentPanel.classList.add("transparent");
+      handleTransition(currentPanel);
+
       if (
         event.target.id === "button-next" &&
         panelContainerPosition > endLeftPosition
       ) {
         panelContainerPosition = panelContainerPosition - wikiboxWidth;
         panelContainer.style.left = `${panelContainerPosition}px`;
+        currentPanel = currentPanel.nextElementSibling;
       } else if (
         event.target.id === "button-previous" &&
         panelContainerPosition < startLeftPosition
       ) {
         panelContainerPosition = panelContainerPosition + wikiboxWidth;
         panelContainer.style.left = `${panelContainerPosition}px`;
+        currentPanel = currentPanel.previousElementSibling;
       }
       togglePagination();
     });
