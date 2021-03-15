85cb1dd1e332d92ad5f01e6c13aae93cd300109d
Schalk Neethling <sneethling@mozilla.com>
2021-03-15 13:42:06 +0200

    manage visibility of pagination buttons

--- js/wikibox.js
+++ js/wikibox.js
@@ -1,30 +1,47 @@
 (function () {
   const wikibox = document.querySelector(".wikibox");
+  const panelContainer = document.querySelector(".panel-container");
+  const numberOfPanels = panelContainer.querySelectorAll(".panel").length;
+  const wikiboxWidth = wikibox.getBoundingClientRect().width;
+  const startLeftPosition = 0;
+  // ignore the initial panel
+  const endLeftPosition = -((numberOfPanels - 1) * wikiboxWidth);
+
+  let panelContainerPosition = 0;
+
+  function togglePagination() {
+    const buttonNext = document.getElementById("button-next");
+    const buttonPrevious = document.getElementById("button-previous");
+
+    if (panelContainerPosition === startLeftPosition) {
+      buttonPrevious.classList.add("hidden");
+    } else {
+      buttonPrevious.classList.remove("hidden");
+    }
+
+    if (panelContainerPosition === endLeftPosition) {
+      buttonNext.classList.add("hidden");
+    } else {
+      buttonNext.classList.remove("hidden");
+    }
+  }
 
   if (wikibox) {
-    const panelContainer = document.querySelector(".panel-container");
-    const numberOfPanels = panelContainer.querySelectorAll(".panel").length;
-    const wikiboxWidth = wikibox.getBoundingClientRect().width;
-    const minLeftPosition = 0;
-    // ignore the initial panel
-    const maxLeftPosition = -((numberOfPanels - 1) * wikiboxWidth);
-
-    let panelContainerPosition = 0;
-
     wikibox.addEventListener("click", (event) => {
       if (
         event.target.id === "button-next" &&
-        panelContainerPosition > maxLeftPosition
+        panelContainerPosition > endLeftPosition
       ) {
         panelContainerPosition = panelContainerPosition - wikiboxWidth;
         panelContainer.style.left = `${panelContainerPosition}px`;
       } else if (
         event.target.id === "button-previous" &&
-        panelContainerPosition < minLeftPosition
+        panelContainerPosition < startLeftPosition
       ) {
         panelContainerPosition = panelContainerPosition + wikiboxWidth;
         panelContainer.style.left = `${panelContainerPosition}px`;
       }
+      togglePagination();
     });
   }
 })();
